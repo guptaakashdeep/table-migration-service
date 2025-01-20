512a667126228615357095cae689e3f67f6aafe2
guptaakashdeep <guptaakashdeep@users.noreply.github.com>
2025-01-20 16:48:22 +0530

    Initial commit for Iceberg Migrator

--- src/core/table_migrator.py
+++ src/core/table_migrator.py
@@ -1,14 +1,15 @@
 """Base Class for Migration Class implementations"""
+
 from abc import ABC, abstractmethod
+from typing import List, Dict, Any
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+from pyspark.sql.utils import AnalysisException
+from pyspark.sql.catalog import Column
 
 
 class TableMigrator(ABC):
     """Abstract base class for table migration implemenations."""
-
-    def __init__(self, spark: SparkSession, table: str):
-        self.spark = spark
-        self.table = table
 
     @abstractmethod
     def validate_source_table(self):
@@ -26,3 +27,145 @@ class TableMigrator(ABC):
         pass
 
 
+class IcebergMigrator(TableMigrator):
+    """Class for migrating Hive Table Format to Iceberg Table Format."""
+    def __init__(
+        self,
+        spark: SparkSession,
+        table: str,
+        catalog: str = "spark_catalog",
+        migration_type: str = "inplace",
+        table_suffix: str = "_iceberg",
+    ):
+        self.spark = spark
+        self.table = table
+        self.catalog = catalog
+        self.full_table_name = f"{self.catalog}.{self.table}"
+        self.migration_type = migration_type
+        self.table_suffix = table_suffix
+
+    def validate_source_table(self) -> None:
+        try:
+            desc_df = self.spark.sql(f"DESC FORMATTED {self.table}")
+            # check if table is not already iceberg
+            provider = desc_df.filter(col("col_name") == "Provider").collect()
+            if provider and provider[0]["data_type"].lower() == "iceberg":
+                raise ValueError(
+                    f"Table {self.full_table_name} is already an iceberg table."
+                )
+        except AnalysisException as e:
+            if "Table or view not found" in str(e):
+                raise ValueError(f"Table {self.full_table_name} does not exist.") from e
+            raise e
+
+    def _get_timestamp_cols(self, col_details: List[Column]) -> List[str]:
+        timestamp_cols = list(
+            filter(lambda column: column.dataType == "timestamp", col_details)
+        )
+        if timestamp_cols:
+            return [field.name for field in timestamp_cols]
+        return []
+
+    def _get_partition_cols(self, col_details: List[Column]) -> List[str]:
+        partition_cols = list(filter(lambda column: column.isPartition, col_details))
+        if partition_cols:
+            return [field.name for field in partition_cols]
+        return []
+
+    def _get_iceberg_table_location(self) -> str:
+        desc_df = self.spark.sql(f"DESC FORMATTED {self.full_table_name}")
+        src_location = desc_df.filter(col("col_name") == "Location").collect()[0][
+            "data_type"
+        ]
+        location, table = src_location.rsplit("/", 1)
+        return f"{location}/iceberg/{table}"
+
+    def validate_migration(self) -> bool:
+        try:
+            self.spark.table(f"{self.full_table_name}.history")
+            return True
+        except Exception:
+            return False
+
+    def migrate_table(self) -> Dict[str, Any]:
+        """Main method for table migration to Iceberg."""
+        # Check if the table is existing and not an Iceberg Table
+        try:
+            self.validate_source_table()
+        except ValueError as e:
+            print("Table is already an iceberg table.")
+            return {"table": self.table,
+                    "migration_status": "Failed",
+                    "error": str(e)}
+
+        try:
+            table_cols_detail = self.spark.catalog.listColumns(self.table)
+            timestamp_cols = self._get_timestamp_cols(table_cols_detail)
+            partition_cols = self._get_partition_cols(table_cols_detail)
+            src_df = self.spark.read.table(self.table)
+
+            # casting timestamp to timestamp_ntz
+            # because Spark timestamp is with timezone specific,
+            # and Iceberg timestamp is without timezone
+            if timestamp_cols:
+                cast_dict = {
+                    cname: col(cname).cast("timestamp_ntz") for cname in timestamp_cols
+                }
+                src_df = src_df.withColumns(cast_dict)
+
+            if partition_cols:
+                partition_col_str = ", ".join(partition_cols)
+            else:
+                partition_col_str = ""
+            mem_table = f"{self.table.split('.')[-1]}_mem"
+            src_df.createOrReplaceTempView(mem_table)
+
+            iceberg_table_location = self._get_iceberg_table_location()
+            iceberg_table_name = f"{self.table}{self.table_suffix}"
+            # CREATE new ICEBERG table without data
+            # with same schema as source table with partitioning
+            if partition_col_str:
+                iceberg_ctas = f"""
+                CREATE TABLE {self.catalog}.{iceberg_table_name} USING iceberg
+                PARTITIONED BY ({partition_col_str})
+                LOCATION '{iceberg_table_location}'
+                AS SELECT * FROM {mem_table} LIMIT 0
+                """
+            else:
+                iceberg_ctas = f"""
+                CREATE TABLE {self.catalog}.{iceberg_table_name} USING iceberg
+                LOCATION '{iceberg_table_location}'
+                AS SELECT * FROM {mem_table} LIMIT 0
+                """
+
+            print("Executing CTAS: \n", iceberg_ctas)
+            # CREATE Iceberg table with suffix
+            self.spark.sql(iceberg_ctas)
+
+            # check if table creation is completed
+            if self.spark.catalog.tableExists(iceberg_table_name):
+                # add_files into iceberg table
+                call_procedure = f"CALL {self.catalog}.system.add_files(table => '{iceberg_table_name}', source_table => '{self.table}')"
+                # TODO: get the output from here into a response object
+                self.spark.sql(call_procedure)
+
+                # drop the actual table
+                self.spark.sql(f"DROP TABLE {self.table}")
+
+                # rename the iceberg table to actual table name
+                self.spark.sql(
+                    f"ALTER TABLE {self.catalog}.{iceberg_table_name} RENAME TO {self.full_table_name}"
+                )
+                # validate migration
+                if self.validate_migration():
+                    return {"table": self.full_table_name,
+                            "migration_status": "Successful"}
+                else:
+                    return {"table": self.full_table_name,
+                            "migration_status": "Failed",
+                            "error": "Migration Validation Failed."}
+            else:
+                raise Exception(f"{iceberg_table_name} Table creation failed.")
+
+        except Exception as e:
+            raise e
