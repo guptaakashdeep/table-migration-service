1b4b0cd2a36724c205c40c77c3d9d9f2798916e8
guptaakashdeep <guptaakashdeep@users.noreply.github.com>
2025-01-21 17:07:47 +0530

    Sample script for using IcebergMigrator to migrate the table from Hive to Iceberg.

--- src/hive_to_iceberg_migration.py
+++ src/hive_to_iceberg_migration.py
@@ -1,13 +1,21 @@
 """Using Migrator Service to migrate from Hive to Iceberg"""
 
 from core.spark_session_factory import IcebergSparkSession
-# from core.table_migrator import IcebergMigrator
+from core.table_migrator import IcebergMigrator
 
 ## Code to migrate tables
 if __name__ == "__main__":
     APP_NAME = "HiveToIcebergMigration"
     WAREHOUSE = "s3://bucket/warehouse"
+    CATALOG = "spark_catalog"
 
+    # Default catalog: 'spark_catalog'
     spark = IcebergSparkSession(
         APP_NAME, WAREHOUSE, catalog_type="glue"
     ).create_spark_session()
+
+    # Name of HIVE Format table that needs to be migrated to Iceberg.
+    TABLE_NAME = "test_db.test_table"
+    migrator = IcebergMigrator(spark, TABLE_NAME, CATALOG)
+    response = migrator.migrate_table()
+    print(response)
